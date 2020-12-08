a397dc61576d4eaac0daac30d647e9c03a35512a
fanglong <337956017@qq.com>
2020-12-09 01:02:24 +0800

    fix: eslint

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,7 +2,7 @@
  * @Author: fangLong
  * @Date: 2020-12-03 00:06:25
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-12-08 23:25:45
+ * @LastEditTime: 2020-12-09 01:01:35
  * @FilePath: \vue3-admin-template\src\shims-vue.d.ts
  */
 declare module '*.vue' {
